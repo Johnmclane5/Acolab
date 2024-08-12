bc9b35d50bc78bca277a03af11fbac6af09f6ddf
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-08-13 03:52:20 +0530

    Update tasks_listener.py

--- bot/helper/listeners/tasks_listener.py
+++ bot/helper/listeners/tasks_listener.py
@@ -463,7 +463,7 @@ class MirrorLeechListener:
                 tmdb_poster_url = await get_movie_poster(movie_name, release_year)
                 new_caption = await remove_extension(name)
                 log_msg = list((await sendMultiMessage(config_dict['MIRROR_LOG_ID'], nmsg + msg, buttonss)).values())[0]
-                await sendMultiMessage(config_dict['CAPTION_LOG_ID'], f'<b>{escape(new_caption)}\n\n<a href={shortlink}>☁️ Cloud Link></a><b>', buttonsss, tmdb_poster_url)
+                await sendMultiMessage(config_dict['MIRROR_LOG_ID'], f'<b>{escape(new_caption)}\n\n<a href={shortlink}>☁️ Cloud Link></a><b>', buttonsss, tmdb_poster_url)
                 if self.linkslogmsg:
                     await deleteMessage(self.linkslogmsg)
             buttons = ButtonMaker()
