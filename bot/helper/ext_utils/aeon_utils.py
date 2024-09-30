c531c6ae7de03035a9198034e36b8049b184f316
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-09-30 12:46:22 +0530

    Update aeon_utils.py

--- bot/helper/ext_utils/aeon_utils.py
+++ bot/helper/ext_utils/aeon_utils.py
@@ -46,22 +46,22 @@ async def get_movie_poster(movie_name, release_year):
 
                     if matching_results:
                         result = matching_results[0]
-                        movie_id = result['id']
-                        media_type = result['media_type']
+                        #movie_id = result['id']
+                        #media_type = result['media_type']
 
                         # Fetch images from the movie/TV show using the /images endpoint
                         # Include no language poster by passing 'null' in include_image_language
-                        tmdb_movie_image_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&include_image_language=null'
+                        #tmdb_movie_image_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&include_image_language=null'
 
-                        async with session.get(tmdb_movie_image_url) as movie_response:
-                            movie_images = await movie_response.json()
+                        #async with session.get(tmdb_movie_image_url) as movie_response:
+                            #movie_images = await movie_response.json()
 
                             # Check if posters exist in the image results
-                            if 'posters' in movie_images and movie_images['posters']:
+                            #if 'posters' in movie_images and movie_images['posters']:
                                 # Use the first available poster (including no-language posters)
-                                poster_path = movie_images['posters'][0]['file_path']
-                                poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
-                                return poster_url
+                                #poster_path = movie_images['posters'][0]['file_path']
+                                #poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                                #return poster_url
 
                         # Fallback: Use the poster_path from the search result
                         if 'poster_path' in result and result['poster_path']:
